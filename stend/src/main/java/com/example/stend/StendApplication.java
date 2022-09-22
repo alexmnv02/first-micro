9de293a23829893a2b071e6070d4f11a859a6955
alex_man <alexmnsbs@gmail.com>
2022-09-22 20:13:51 +0300

    init

---
+++ stend/src/main/java/com/example/stend/StendApplication.java
@@ -0,0 +1,13 @@
+package com.example.stend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StendApplication.class, args);
+    }
+
+}
---
+++ stend/src/test/java/com/example/stend/StendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
